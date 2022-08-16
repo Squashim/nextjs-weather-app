443efaa20cbd110fc88b0573b03d82b00a658cf0
Squashim <konrad18v1@wp.pl>
2022-08-16 12:15:57 +0200

    deploy

---
+++ components/Spinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import spinner from "../public/spinner.gif";
+
+const Spinner = () => {
+	return (
+		<>
+			<Image alt='loading' src={spinner} className='w-[200px] m-auto block' />
+		</>
+	);
+};
+
+export default Spinner;
